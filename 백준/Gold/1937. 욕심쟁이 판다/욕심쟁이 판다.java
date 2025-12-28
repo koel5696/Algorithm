191d17330f12a0bc1ec88455bdcee3dbc1e40d3a
koel5696 <128348671+koel5696@users.noreply.github.com>
2025-12-28 20:10:08 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 496 ms, Memory: 37480 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] x = {-1, 1, 0, 0};
+    static int[] y = {0, 0, -1, 1};
+    static int[][] graph;
+    static int[][] dp;
+    static boolean[][] visited;
+    static int n;
+    static int max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        graph = new int[n + 1][n + 1];
+        dp = new int[n + 1][n + 1];
+        visited = new boolean[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = 1;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                max = Math.max(max, dfs(i, j, 1));
+            }
+        }
+        bw.write(max + "");
+        bw.flush();
+    }
+
+    public static int dfs(int currX, int currY, int count) {
+        int result = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x[i] + currX;
+            int ny = y[i] + currY;
+            if ((nx > 0 && ny > 0) && (nx <= n && ny <= n)) {
+                if (graph[nx][ny] > graph[currX][currY]) {
+                    if (dp[nx][ny] > 1) {
+                        result = Math.max(result, dp[nx][ny] + 1);
+                        continue;
+                    }
+                    result = Math.max(result, dfs(nx, ny, count + 1) + 1);
+                }
+            }
+        }
+
+        dp[currX][currY] = result;
+        return dp[currX][currY];
+    }
+}
