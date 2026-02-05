0babde1b76a20ed9acab49d964f38dc5acb8f1b4
koel5696 <128348671+koel5696@users.noreply.github.com>
2026-02-05 18:59:28 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ugb.TITLE, ugb.BOARD_ID, ugr.REPLY_ID, ugr.WRITER_ID, ugr.CONTENTS,
+DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+from USED_GOODS_BOARD ugb
+join USED_GOODS_REPLY ugr
+on ugb.BOARD_ID = ugr.BOARD_ID
+where ugb.CREATED_DATE >= '2022-10-01' && ugb.CREATED_DATE < '2022-11-01'
+order by ugr.CREATED_DATE ASC, ugb.TITLE ASC
