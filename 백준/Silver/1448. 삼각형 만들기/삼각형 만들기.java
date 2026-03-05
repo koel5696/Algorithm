9ab8d3baf2a16756be4e94cb489a23d34b5d9c80
koel5696 <128348671+koel5696@users.noreply.github.com>
2026-03-05 22:51:50 +0900

    [Silver III] Title: 삼각형 만들기, Time: 1648 ms, Memory: 107960 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -6,10 +6,6 @@ import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-    static List<List<int[]>> list = new ArrayList<>();
-    static int[] dijkstra;
-    static int[] items;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -20,12 +16,12 @@ public class Main {
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr, (a, b) -> b - a);
+        Arrays.sort(arr);
 
         int result = 0;
-        for(int i = 0; i < n-2; i++) {
-            if(arr[i+1] + arr[i+2] > arr[i]) {
-                result = arr[i] + arr[i+1] + arr[i+2];
+        for(int i = n-1; i > 1; i--) {
+            if(arr[i-1] + arr[i-2] > arr[i]) {
+                result = arr[i] + arr[i-1] + arr[i-2];
                 break;
             }
         }
