060c75a5ebc1470ece51f992e4e5afd406e6cad3
koel5696 <128348671+koel5696@users.noreply.github.com>
2026-06-17 19:14:25 +0900

    [D4] Title: 게으름뱅이, Time: 1,387 ms, Memory: 167,128 KB -BaekjoonHub

---
+++ SWEA/D4/26793. 게으름뱅이/README.md
@@ -0,0 +1,15 @@
+# [D4] 게으름뱅이 - 26793 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ3XsxFqSCvHBIPV) 
+
+### 성능 요약
+
+메모리: 167,128 KB, 시간: 1,387 ms, 코드길이: 2,732 Bytes
+
+### 제출 일자
+
+2026-06-17 19:14
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/26793. 게으름뱅이/게으름뱅이.java
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.*;
+import java.util.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	 public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+
+            int[][] time = new int[m][2];
+            for (int j = 0; j < m; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                time[j][0] = Integer.parseInt(st.nextToken());
+                time[j][1] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(time, (a, b) -> Integer.compare(a[1], b[1]));
+            for (int j = 1; j < m; j++) {
+                time[j][0] += time[j - 1][0];
+            }
+
+            int result = Integer.MAX_VALUE;
+
+            for (int j = 0; j < m; j++) {
+                result = Math.min(result, time[j][1] - time[j][0]);
+            }
+            bw.write(result + "\n");
+        }
+        bw.flush();
+    }
+}
